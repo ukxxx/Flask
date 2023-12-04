2af4655e8e83b4d8e1486eec4da11010c30ee357
uk <mail@mail.ru>
2023-12-04 18:58:46 +0300

    delete old server and client

--- client.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.post('http://127.0.0.1:8080/hello?name=John&age=20',
-                         json={"name": "user1", "password": "1234"},
-                         headers={"token": "egdfgsgasdfafadf"}
-                         )
-print(response.text)
-print(response.status_code)
